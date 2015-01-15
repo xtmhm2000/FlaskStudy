c59c566e0a7050aa67ac75c54373dd5c13be3334
ZF <xtmhm2000@163.com>
2015-01-15 16:37:25 +0800

    添加模板

--- FlaskStudy.py
+++ FlaskStudy.py
@@ -1,12 +1,18 @@
-from flask import Flask
-
+from flask import Flask,render_template
+from flask import request
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World!</h1>'
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return render_template('index.html')
 
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
