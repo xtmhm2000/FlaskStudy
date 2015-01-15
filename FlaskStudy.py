0545647e71cef6b4001ecae6675f22edf824e50a
ZF <xtmhm2000@163.com>
2015-01-15 17:53:11 +0800

    第3 模板

--- FlaskStudy.py
+++ FlaskStudy.py
@@ -1,10 +1,11 @@
 from flask import Flask,render_template
 from flask import request
 from flask.ext.bootstrap import Bootstrap
-
+from flask.ext.moment import Moment
+from datetime import datetime
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -16,8 +17,8 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return render_template('index.html')
+  return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 
 
