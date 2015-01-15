e70a2ab6dc960cccc1c42f2de97e5e9643beec52
ZF <xtmhm2000@163.com>
2015-01-15 18:21:32 +0800

    第3 模板

--- FlaskStudy.py
+++ FlaskStudy.py
@@ -3,10 +3,23 @@ from flask import request
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    passowrd = StringField('What is your password?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
+app.config['SECRET_KEY'] = 'hard to guess string'
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -15,10 +28,16 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html',
-                           current_time=datetime.utcnow())
+    name = None
+    passowrd=None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        passowrd=form.passowrd.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name,password=passowrd)
 
 
 
@@ -28,5 +47,9 @@ def user(name):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
 
 
