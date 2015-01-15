8b576548514e497b1e5a2f3382b381e36e99d67c
zf <xtmhm2000@163.com>
2015-01-15 22:27:38 +0800

    第4 数据库

--- FlaskStudy.py
+++ FlaskStudy.py
@@ -1,5 +1,7 @@
-from flask import Flask,render_template
+import os
+from flask import Flask,render_template,session,flash,redirect,url_for
 from flask import request
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
@@ -7,6 +9,7 @@ from datetime import datetime
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask.ext.script import Manager
 
 
 
@@ -16,9 +19,50 @@ class NameForm(Form):
     submit = SubmitField('Submit')
 
 
+
+
+
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+manager = Manager(app)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
+
+
+
 app.config['SECRET_KEY'] = 'hard to guess string'
 @app.errorhandler(404)
 def page_not_found(e):
@@ -30,14 +74,21 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
-    passowrd=None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        passowrd=form.passowrd.data
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
         form.name.data = ''
-    return render_template('index.html', form=form, name=name,password=passowrd)
+        return redirect(url_for('index'))
+    return render_template('index.html',
+        form = form, name = session.get('name'),
+        known = session.get('known', False))
 
 
 
@@ -46,7 +97,7 @@ def user(name):
     return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
 
 
 
